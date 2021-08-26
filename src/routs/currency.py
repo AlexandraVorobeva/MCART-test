888aab1f180407918dd0d0282b709e0a8d5f4b72
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-08-26 13:32:44 +0300

    1

--- src/routs/currency.py
+++ src/routs/currency.py
@@ -1,14 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import datetime
 from ..services.currency import fetch_all_currencies, compare_currency
+import functools
+from requests_cache import CachedSession
+import requests_cache
 
+
+session = CachedSession()
+requests_cache.install_cache(expire_after=360, allowable_methods=('GET', 'POST'))
 
 router = APIRouter(prefix="/currency")
 
 
 @router.get("/all")
 def get_list_of_all_currencies():
-    """List of currencies of the world."""
-    return fetch_all_currencies()
+        return fetch_all_currencies()
 
 
 @router.get("/difference")
@@ -19,4 +25,9 @@ def get_rub_difference(character_code_of_currency: str, day1: str, day2: str):
 
     Currency must be sent like character code: USD, EUR.
     """
+    try:
+        datetime.datetime.strptime(day2, '%Y-%m-%d') and datetime.datetime.strptime(day2, '%Y-%m-%d')
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Incorrect format of 'days'")
     return compare_currency(character_code_of_currency, day1, day2)
+
